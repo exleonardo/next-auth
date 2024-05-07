043f23fe8a4be97040fbcaf0ce4daa3d0cc85d58
Alexander Khomenok <exleonardo@mail.ru>
2024-05-07 10:49:04 +0300

    feat: add auth

--- components/auth/ui/auth-form.tsx
+++ components/auth/ui/auth-form.tsx
@@ -1,25 +1,55 @@
-import { useState } from 'react'
+import { FormEvent, useRef, useState } from 'react'
 
 import s from '../style/auth-form.module.scss'
 
+const createUser = async (email: string, password: string) => {
+  const response = await fetch('/api/auth/signup', {
+    body: JSON.stringify({ email, password }),
+    headers: { 'Content-type': 'application/json' },
+    method: 'POST',
+  })
+  const data = await response.json()
+
+  if (!response.ok) {
+    throw new Error(data.message || 'Something went wrong')
+  }
+
+  return data
+}
+
 export const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true)
-
-  function switchAuthModeHandler() {
+  const emailInputRef = useRef<HTMLInputElement>(null)
+  const passwordInputRef = useRef<HTMLInputElement>(null)
+  const switchAuthModeHandler = () => {
     setIsLogin(prevState => !prevState)
+  }
+
+  const submitHandler = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const enteredEmail = emailInputRef.current.value
+    const enteredPassword = passwordInputRef.current.value
+
+    if (isLogin) {
+      //some
+    } else {
+      try {
+        const result = await createUser(enteredEmail, enteredPassword)
+      } catch (err) {}
+    }
   }
 
   return (
     <section className={s.auth}>
       <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className={s.control}>
           <label htmlFor={'email'}>Your Email</label>
-          <input id={'email'} required type={'email'} />
+          <input id={'email'} ref={emailInputRef} required type={'email'} />
         </div>
         <div className={s.control}>
           <label htmlFor={'password'}>Your Password</label>
-          <input id={'password'} required type={'password'} />
+          <input id={'password'} ref={passwordInputRef} required type={'password'} />
         </div>
         <div className={s.actions}>
           <button>{isLogin ? 'Login' : 'Create Account'}</button>

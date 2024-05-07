41e08162bf3d94e88156f0ba5983ee0ebf931020
Alexander Khomenok <exleonardo@mail.ru>
2024-05-07 10:49:17 +0300

    feat: add getLayout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
+import { getLayout } from '@/components/layout/main-navigation'
 import { StartingPageContent } from '@/components/starting-page'
 
 function HomePage() {
   return <StartingPageContent />
 }
-HomePage.getLayout = HomePage
+HomePage.getLayout = getLayout
 export default HomePage

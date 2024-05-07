ee8ff03f91c54e4d877d665a06d1d300cf9d8c6a
Alexander Khomenok <exleonardo@mail.ru>
2024-05-07 10:49:44 +0300

    feat: add route

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -1,8 +1,35 @@
+import { hashPassword } from '@/helpers/auth'
 import { connectDatabase } from '@/helpers/db-util'
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const client = await connectDatabase()
+  const data = req.body
+  const { email, password } = data
 
-  client.db()
+  if (req.method === 'POST') {
+    if (
+      !email.includes('@') ||
+      !password ||
+      password.trim().length < 7 ||
+      !email ||
+      email.trim() === ''
+    ) {
+      res
+        .status(422)
+        .json({ message: 'Invalid input - password should be at least 7 characters long.' })
+
+      return
+    }
+
+    const client = await connectDatabase()
+    const db = client.db()
+    const hashedPassword = await hashPassword(password)
+
+    const result = await db.collection('users').insertOne({
+      email,
+      password: hashedPassword,
+    })
+
+    res.status(201).json({ message: 'Created user!' })
+  }
 }
